f91184b1a2c7847c72da60ad96986639be93dfdd
mr.chaiwat chainvong <65877075+nui4328@users.noreply.github.com>
2023-07-11 07:12:51 +0700

    Update test.ino

--- line_robot/test.ino
+++ line_robot/test.ino
@@ -1,30 +1,122 @@
-void fw_07(int spl, int spr, float kp, int sensor, int ofset)
+void fline_2sensor(int sl, int sr, float kp, int tm, int sensor, char sp, int ofset)
   {
-    while(1)
+
+    if(tm > 0)
       {
-         PID_output = (kp * error_F()) + (0.00015 * I) + (kp * D);
-         Motor(spl - PID_output, spr + PID_output); 
-         if(sensor == 0)
-            {
-              if(mcp_f(0)< md_mcp_f(0))
-                {
-                  break;
-                }                
-            }
-         else if(sensor == 7)
-            {
-              if(mcp_f(7)< md_mcp_f(7))
-                {
-                  break;
-                } 
-            }
+        unsigned long lasts_time = millis();
+        while(millis() - lasts_time < tm)
+          {
+            float error_L = map(mcp_f(3), min_mcp_f(3), max_mcp_f(3), 0, 30 );
+            float error_R = map(mcp_f(4), min_mcp_f(4), max_mcp_f(4), 0, 30 );
+            errors = error_L - error_R;  
+            I = 0;
+            previous_I = 0;
+            previous_error = 0;
+            P = errors;
+            I = I + previous_I;
+            D = errors - previous_error ;            
+            previous_I=I;
+            previous_error=errors  ;  
+            PID_output = (kp * P) + (0.00015 * I) + (kp* D); 
+
+            Motor(sl + PID_output, sr - PID_output);
+            //Serial.println(errors);
+  
+          }
+          
       }
-    if(ofset > 0)
+    else
       {
-        Motor(-spl, -spr);
+        while(1)
+          {
+            float error_L = map(mcp_f(3), min_mcp_f(3), max_mcp_f(3), 0, 30 );
+            float error_R = map(mcp_f(4), min_mcp_f(4), max_mcp_f(4), 0, 30 );
+            errors = error_L - error_R;  
+            I = 0;
+            previous_I = 0;
+            previous_error = 0;
+            P = errors;
+            I = I + previous_I;
+            D = errors - previous_error ;            
+            previous_I=I;
+            previous_error=errors  ;  
+            PID_output = (kp * P) + (0.00015 * I) + (kp* D); 
+
+            Motor(sl + PID_output, sr - PID_output);
+            //Serial.println(errors);
+            if(sensor == 0)
+                  {
+                      if(mcp_f(0)<md_mcp_f(0))
+                          {
+                              break;
+                          }
+                  }
+              else if(sensor == 7)
+                  {
+                      if(mcp_f(7)<md_mcp_f(7))
+                          {
+                              break;
+                          }
+                  }
+  
+          }
+      }
+
+ ///////////////////////////////////////////////////////////////     
+    if(sp == 's')
+      {
+        Motor(-sl, -sr);
         delay(ofset);
         Motor(0, 0);
         delay(50);
       }
-     else{}
+    else
+      {
+        
+        while(1)
+          {
+            float error_L = map(mcp_f(3), min_mcp_f(3), max_mcp_f(3), 0, 30 );
+            float error_R = map(mcp_f(4), min_mcp_f(4), max_mcp_f(4), 0, 30 );
+            errors = error_L - error_R;  
+            I = 0;
+            previous_I = 0;
+            previous_error = 0;
+            P = errors;
+            I = I + previous_I;
+            D = errors - previous_error ;            
+            previous_I=I;
+            previous_error=errors  ;  
+            PID_output = (kp * P) + (0.00015 * I) + (kp* D); 
+
+            Motor(sl + PID_output, sr - PID_output);
+            //Serial.println(errors);
+            if(mcp_f(sensor)<md_mcp_f(sensor))
+                  {
+                    break;
+                  }
+              
+          }
+          
+        unsigned long lasts_time = millis();
+        while(millis() - lasts_time < 50)
+          {
+            float error_L = map(mcp_f(3), min_mcp_f(3), max_mcp_f(3), 0, 30 );
+            float error_R = map(mcp_f(4), min_mcp_f(4), max_mcp_f(4), 0, 30 );
+            errors = error_L - error_R;  
+            I = 0;
+            previous_I = 0;
+            previous_error = 0;
+            P = errors;
+            I = I + previous_I;
+            D = errors - previous_error ;            
+            previous_I=I;
+            previous_error=errors  ;  
+            PID_output = (kp * P) + (0.00015 * I) + (kp* D); 
+
+            Motor(sl + PID_output, sr - PID_output);
+            //Serial.println(errors);
+  
+          }
+      }
+ //////////////////////////////////////////////////////////////     
   }
