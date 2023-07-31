b0fee86d4ad8abb031f529daec042cda7ad89d1d
mr.chaiwat chainvong <65877075+nui4328@users.noreply.github.com>
2023-07-31 17:02:54 +0700

    Update my_RPboard.ino

--- examples/my_RPboard/my_RPboard.ino
+++ examples/my_RPboard/my_RPboard.ino
@@ -5,13 +5,10 @@ void setup()
   {
     Serial.begin(9600);
     sensor_set();
-    //setup_mpu();
-    //calibration_Yak();
     mydisplay("MY-MAKERS", 20, 30, 2, white, black);
     
     sw();
     
-    //calibration_Yak();
     mydisplay("MY-MAKERS", 20, 30, 2, white, black);
   }
 
