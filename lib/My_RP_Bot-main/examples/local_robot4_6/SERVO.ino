46543c497f6ca5fa530198a418a542c948d21d9a
mr.chaiwat chainvong <65877075+nui4328@users.noreply.github.com>
2023-09-10 11:16:43 +0700

    Add files via upload

---
+++ lib/My_RP_Bot-main/examples/local_robot4_6/SERVO.ino
@@ -0,0 +1,44 @@
+ void servo_mission() 
+    {                         
+      servo(23,120);  //หุบ      
+      delay(300); 
+          
+      servo(8,servo_up );  //ยก
+      delay(200); 
+      check_box();
+
+    }
+void servo_store() // วางพื้นขาวดำ
+    {    
+       servo(8,servo_down );  //ลง   
+       delay(400);
+       slow_servo_open();
+       delay(200);
+       servo(8,servo_up);  //ลง
+       delay(300);
+    }
+
+  void down_servo() // วาง
+    {
+       servo(8,servo_down );  //ลง
+       servo(23,60);
+       delay(200);
+    }
+ 
+ void slow_servo_down()
+  {
+    for(int i = servo_up; i > servo_down + 10; i-- )
+      {
+        servo(8, i);
+        delay(6);
+      }
+  }
+  
+ void slow_servo_open()
+  {
+    for(int i = 120; i > 90; i-- )
+      {
+        servo(23, i);
+        delay(10);
+      }
+  }
