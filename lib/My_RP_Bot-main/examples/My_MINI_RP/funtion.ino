15ff4cd5b2571f50ec6645b1eb38fac9e7996557
mr.chaiwat chainvong <65877075+nui4328@users.noreply.github.com>
2024-09-23 15:55:49 +0700

    Add files via upload

---
+++ lib/My_RP_Bot-main/examples/My_MINI_RP/funtion.ino
@@ -0,0 +1,44 @@
+void test_bord()
+  {
+    for(int i=0; i<3; i++)
+      {
+        servo(0, 160); servo(28, 160); servo(1, 160);delay(1000);
+        servo(0, 10);servo(28, 10); servo(1, 10); delay(1000);
+      }
+    bz(200);
+
+    while(1)
+      {
+        Serial.print("Encoder_L: ");
+        Serial.print(encoder.Poss_L());
+        Serial.print("     ");
+        Serial.print("Encoder_R: ");
+        Serial.println(encoder.Poss_R());
+    
+        delay(10);  // Delay for better readability in serial out
+        if(digitalRead(9) == 0)
+          {
+            break;
+          }
+      }
+    bz(200);
+    while(1)
+      {
+        Motor(30, 30);delay(1000);
+        Motor(-30, -30);delay(1000);
+        Motor(0, 0);delay(1000);
+        
+      }
+    bz(200);
+  }
+  
+void encoder_poss()
+  {    
+    Serial.print("Encoder_L: ");
+    Serial.print(encoder.Poss_L());
+    Serial.print("     ");
+    Serial.print("Encoder_R: ");
+    Serial.println(encoder.Poss_R());
+
+    delay(100);  // Delay for better readability in serial out
+  }
