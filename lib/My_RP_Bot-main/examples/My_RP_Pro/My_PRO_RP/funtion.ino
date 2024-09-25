5ec7b33c043de9ba50eece9e2864202f3e225f73
mr.chaiwat chainvong <65877075+nui4328@users.noreply.github.com>
2024-09-25 18:52:00 +0700

    Add files via upload

---
+++ lib/My_RP_Bot-main/examples/My_RP_Pro/My_PRO_RP/funtion.ino
@@ -0,0 +1,11 @@
+
+void encoder_poss()
+  {    
+    Serial.print("Encoder_L: ");
+    Serial.print(encoder.Poss_L());
+    Serial.print("     ");
+    Serial.print("Encoder_R: ");
+    Serial.println(encoder.Poss_R());
+
+    delay(100);  // Delay for better readability in serial out
+  }
