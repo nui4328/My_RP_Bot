660ae74983996d12836559440d395449df88501e
mr.chaiwat chainvong <65877075+nui4328@users.noreply.github.com>
2025-04-30 14:23:29 +0700

    Update my_Pico_opt2025.ino

--- libraries/My_RP_Bot-main/examples/my_Pico_opt2025/my_Pico_opt2025.ino
+++ libraries/My_RP_Bot-main/examples/my_Pico_opt2025/my_Pico_opt2025.ino
@@ -10,7 +10,7 @@ void setup()
       Add_rear_sensor(false);    //------>> ไม่ติดตั้งเซนเซอร์หลัง
 
       
-      arm_open_up();
+      //arm_open_up();
       //arm_down_close();
       sw();    //------>> คำสั่งรอกดปุ่ม start
 
@@ -18,7 +18,49 @@ void setup()
       
     //--------------------------------------------->>  เอาคำสั่งต่าง ๆ มา RUN ตรงนี้
 
-    fw_distance(15, 15, 0.55, 500, 10);
+for(int i=0; i<15; i++)
+  {
+    fw_line(30, 30, 0.25, 's', "a5", 20);
+    turn_left_sensor(3, 54, "a2", 20); 
+
+    fw_line(30, 30, 0.25, 's', "a5", 20);
+    turn_right_sensor(2, 54, "a3", 20);
+
+    fw_line(30, 30, 0.25, 's', "a5", 20);
+    turn_right_sensor(2, 54, "a3", 20);
+    
+    fw_line(30, 30, 0.25, 's', "a0", 20);
+    turn_left_sensor(2, 54, "a2", 20); 
+    
+    fw_line(30, 30, 0.75, 's', "a5", 20);
+    turn_right_sensor(2, 34, "a3", 20);
+
+    fw_line(30, 30, 0.35, 's', "a0", 20);
+    turn_left_sensor(3, 34, "a2", 20); 
+   
+    fw_line(30, 30, 0.45, 'p', "a0", 0);
+
+    fw_line(30, 30, 0.35, 's', "a0", 20);
+    turn_right_sensor(5, 34, "a3", 20); 
+
+
+     fw_line(50, 50, 0.35, 'p', "a0", 0);
+
+     fw_line(30, 30, 0.35, 's', "a5", 20);
+     turn_right_sensor(2, 34, "a3", 20); 
+
+     fw_line(30, 30, 0.85, 's', "a5", 20);
+     turn_right_sensor(3, 34, 60, 20); 
+
+     fw_distance(70, 70, 0.30, 25, 0);
+     fw_line(20, 20, 0.25, 's', "a5", 20);
+     turn_right_wheel(5, 34, "a3", 20); 
+  }
+    
+    
+    
+    
+ 
 
      // can_1();
      // can_2();
@@ -34,9 +76,10 @@ void setup()
     
 void loop() 
   {
-    /* 
+    /*
     Serial.print(encoder.Poss_L());
     Serial.print("  ");
     Serial.println(encoder.Poss_R());
+    delay(10);
     */
   }
