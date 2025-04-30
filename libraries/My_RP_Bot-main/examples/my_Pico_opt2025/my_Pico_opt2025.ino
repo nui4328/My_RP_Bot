71b4ba5f4421b5e12ba2e69cccd989f0a3943459
mr.chaiwat chainvong <65877075+nui4328@users.noreply.github.com>
2025-04-30 08:45:48 +0700

    Update my_Pico_opt2025.ino

--- libraries/My_RP_Bot-main/examples/my_Pico_opt2025/my_Pico_opt2025.ino
+++ libraries/My_RP_Bot-main/examples/my_Pico_opt2025/my_Pico_opt2025.ino
@@ -1,24 +1,39 @@
 
 #include <my_RPOPT_mini.h>
 
+
 void setup() 
   {
       setup_robot();
       to_set_motor_LR(90, 90);  //------->> ตั้งค่ามอเตอร์ถ้าหุ่นยนต์วิ่งไม่ตรง
-    
+
+      arm_open_up();
+      //arm_down_close();
       sw();    //------>> คำสั่งรอกดปุ่ม start
 
-
+      
+      
     //--------------------------------------------->>  เอาคำสั่งต่าง ๆ มา RUN ตรงนี้
+
+  
+
+     // can_1();
+     // can_2();
+     // can_3();
+
     
-    can_1();
-    can_2();
-
-
 
     //--------------------------------------------->>  เอาคำสั่งต่าง ๆ มา RUN ตรงนี้
     //--------------------------------------------->>
+    encoder.resetEncoders();
   }
     
     
-void loop() {}
+void loop() 
+  {
+    /* 
+    Serial.print(encoder.Poss_L());
+    Serial.print("  ");
+    Serial.println(encoder.Poss_R());
+    */
+  }
