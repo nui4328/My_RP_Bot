9f5a1b54cb1fb96c9c494a3a325a5b67439d1506
mr.chaiwat chainvong <65877075+nui4328@users.noreply.github.com>
2025-05-01 13:28:49 +0700

    Update MyRP_OPT123_2025.ino

--- libraries/My_RP_Bot-main/examples/MyRP_OPT123_2025/MyRP_OPT123_2025.ino
+++ libraries/My_RP_Bot-main/examples/MyRP_OPT123_2025/MyRP_OPT123_2025.ino
@@ -1,9 +1,11 @@
 #include <my2wd_encoderOPT.h>
 //---------------------------------\\
 
+
 void setup()
   {
-    set_pid_moveLR(1.45, 0.0001, 0.035);   //------->> ตั้งค่า pid สำหรับหมุนตัว
+    set_pid_moveLR(1.35, 0.00015, 0.040);   //------->> ตั้งค่า pid สำหรับหมุนตัว
+    set_pid_chopsticks(0.6, 0.00001, 0.15); //------->> ตั้งค่า pid สำหรับขึ้นตะเกียบ
     setup_OPT();
     pinMode(20, INPUT_PULLUP);
     
@@ -16,17 +18,61 @@ void setup()
     delay(100); 
     servo(27, servo_27_close);   
     servo(28, servo_28_close);
+    servo(29, 90);
     robot_start();
     eep_to_code();
-    Serial.println(my.gyro('z')); delay(10);
+    
 
     //reset_arm();
     ///----------------------------------------------------->> เขียนโค๊ดที่นี้
+    
+
+   for(int i=0; i<5; i++)
+    {
+     fw(50, 50, 0.85, 90, "none_line");
+
+     moveLR(75, 90);
+
+     fw(60, 60, 0.35, 50, "line");
+     moveLR(75, 90);
+
+    fw(50, 50, 0.35, 25, "line");
+     moveLR(75, 90);
+
+     fw(50, 50, 0.35, 26, "none_line");
+     moveLR(80, -90);
+     set_b(1);
+     fw(50, 50, 0.35, 25, "none_line");
+     moveLR(80, -90);
+     set_b(5);
+     fw_bridge(50, 50, 0.55, 175, "line");
+     moveLR(60, -185);
+     set_b(3);
+
+     fw_bridge(70, 70, 0.35, 170, "line");
+     moveLR(75, 90);
+
+     fw(50, 52, 0.35, 25, "line");
+     moveLR(80, 90);
+
+     fw(50, 50, 0.35, 25, "none_line");
+     moveLR(80, -90);
+
+     fw(50, 50, 0.35, 25, "line");
+     moveLR(75, -90);
+     set_b(1);
+
+     fw(70, 70, 0.35, 50, "line");
+     moveLR(75, -90);
+     set_b(1);
+
+     fw(70, 70, 0.35, 90, "line");
+
+     moveLR(60, -180);
+     set_b(3);
+    }
+
    
-
-     
-
-
 
     ///----------------------------------------------------->> เขียนโค๊ดที่นี้
     ///----------------------------------------------------->> เขียนโค๊ดที่นี้
@@ -34,8 +80,9 @@ void setup()
   }
 
 void loop() {
-
+  
+   
   //Serial.print(digitalRead(20));Serial.println("  ");  //อ่านค่าลิมิตสวิทย์
  //Serial.print(analogRead(26));Serial.print("  ");      //อ่านค่าวัดระย
-  //Serial.println(my.gyro('z')); delay(10);              //อ่านค่า gyro
+  Serial.println(my.gyro('z')); delay(10);              //อ่านค่า gyro
 }
