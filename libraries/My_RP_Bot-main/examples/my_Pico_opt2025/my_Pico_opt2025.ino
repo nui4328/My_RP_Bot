70e64b9145868eedcd755cf1f6ca7e565c9af99f
mr.chaiwat chainvong <65877075+nui4328@users.noreply.github.com>
2025-05-12 12:20:51 +0700

    Update my_Pico_opt2025.ino

--- libraries/My_RP_Bot-main/examples/my_Pico_opt2025/my_Pico_opt2025.ino
+++ libraries/My_RP_Bot-main/examples/my_Pico_opt2025/my_Pico_opt2025.ino
@@ -1,6 +1,8 @@
 
 #include <my_RPOPT_mini.h>
 
+int servo_down = 50;      //-------------------->> ตั้งค่า มื่อจับลงขนานกับพื้น
+int servo_close = 130;    //-------------------->> ตั้งค่า ฝ่ามือให้ปลายเข้ามาแตะกันพอดี
 
 void setup() 
   {
@@ -10,61 +12,25 @@ void setup()
       Add_rear_sensor(false);    //------>> ไม่ติดตั้งเซนเซอร์หลัง
 
       
-      //arm_open_up();
+      arm_open_up();
       //arm_down_close();
       sw();    //------>> คำสั่งรอกดปุ่ม start
 
       
       
     //--------------------------------------------->>  เอาคำสั่งต่าง ๆ มา RUN ตรงนี้
-
-for(int i=0; i<15; i++)
-  {
-    fw_line(30, 30, 0.25, 's', "a5", 20);
-    turn_left_sensor(3, 54, "a2", 20); 
-
-    fw_line(30, 30, 0.25, 's', "a5", 20);
-    turn_right_sensor(2, 54, "a3", 20);
-
-    fw_line(30, 30, 0.25, 's', "a5", 20);
-    turn_right_sensor(2, 54, "a3", 20);
-    
-    fw_line(30, 30, 0.25, 's', "a0", 20);
-    turn_left_sensor(2, 54, "a2", 20); 
-    
-    fw_line(30, 30, 0.75, 's', "a5", 20);
-    turn_right_sensor(2, 34, "a3", 20);
-
-    fw_line(30, 30, 0.35, 's', "a0", 20);
-    turn_left_sensor(3, 34, "a2", 20); 
-   
-    fw_line(30, 30, 0.45, 'p', "a0", 0);
-
-    fw_line(30, 30, 0.35, 's', "a0", 20);
-    turn_right_sensor(5, 34, "a3", 20); 
-
-
-     fw_line(50, 50, 0.35, 'p', "a0", 0);
-
-     fw_line(30, 30, 0.35, 's', "a5", 20);
-     turn_right_sensor(2, 34, "a3", 20); 
-
-     fw_line(30, 30, 0.85, 's', "a5", 20);
-     turn_right_sensor(3, 34, 60, 20); 
-
-     fw_distance(70, 70, 0.30, 25, 0);
-     fw_line(20, 20, 0.25, 's', "a5", 20);
-     turn_right_wheel(5, 34, "a3", 20); 
-  }
-    
-    
-    
-    
  
 
-     // can_1();
-     // can_2();
-     // can_3();
+    //can_1();
+    //can_2();
+    //can_3();
+    //can_4();
+    //can_5();
+   // can_6();
+
+
+       
+
 
     
 
@@ -76,6 +42,8 @@ for(int i=0; i<15; i++)
     
 void loop() 
   {
+    Serial.println(my.gyro('z')); delay(10);   
+    
     /*
     Serial.print(encoder.Poss_L());
     Serial.print("  ");
