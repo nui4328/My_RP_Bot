915a9bc661e3fbde86d3b4a2edfb04818d97d4cd
mr.chaiwat chainvong <65877075+nui4328@users.noreply.github.com>
2025-05-17 12:45:58 +0700

    Update example.ino

--- libraries/My_RP_Bot-main/examples/MyRP_OPT123_2025/example.ino
+++ libraries/My_RP_Bot-main/examples/MyRP_OPT123_2025/example.ino
@@ -1,1 +1,62 @@
 
+void st1()  
+  {
+    fw(50, 50, 1.5, 90, "none_line"); 
+    moveLR(70, 90); 
+    fw(50, 50, 1.5, 60, "none_line"); 
+    moveLR(70, 90); 
+    set_b(1);
+    fw(50, 50, 1.5, 26, "none_line"); 
+    moveLR(70, 90); 
+    fw(50, 50, 1.5, 25, "line"); 
+    moveLR(70, -90); 
+    set_b(1);
+    fw(50, 50, 1.5, 30, "none_line"); 
+    moveLR(70, -90); 
+    set_b(1);
+    fw_bridge(50, 50, 1.5, 120, "none_line");  
+    moveLR(70, 90); 
+
+    fw(50, 50, 1.5, 30, "line");
+    moveLR(70, 90); 
+    set_b(1);
+
+    fw_chopsticks (30, 30, 2.5, 60, "none_line");
+    fw(50, 50, 1.5, 30, "line");
+    
+    set_f(2);
+    bw(50, 50, 1.5, 40, "none_line");
+    bw_chopsticks (30, 30, 2.0, 60, "line");
+
+    moveLR(70, 90); 
+    set_b(1);
+
+    fw(40, 40, 1.5, 27, "none_line"); 
+    moveLR(70, -90); 
+
+    fw_bridge(50, 50, 1.5, 120, "line");  
+    moveLR(70, 90); 
+
+    fw(50, 50, 1.5, 25, "line");  
+    moveLR(70, 90); 
+    fw(50, 50, 1.5, 25, "none_line");  
+    moveLR(70, -90); 
+
+    fw(50, 50, 1.5, 30, "line");  
+    moveLR(70, -90); 
+
+    set_b(1);
+
+    fw(40, 40, 1.5, 60, "none_line"); 
+    moveLR(70, -90);
+
+    set_b(1);
+
+    fw(40, 40, 1.5, 90, "line"); 
+  
+
+
+
+
+
+  }
