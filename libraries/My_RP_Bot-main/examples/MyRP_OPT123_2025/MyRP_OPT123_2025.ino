a85cf48e1c27ac26b1393c220a0abee8771d43d8
mr.chaiwat chainvong <65877075+nui4328@users.noreply.github.com>
2025-05-31 11:08:33 +0700

    Update MyRP_OPT123_2025.ino

--- libraries/My_RP_Bot-main/examples/MyRP_OPT123_2025/MyRP_OPT123_2025.ino
+++ libraries/My_RP_Bot-main/examples/MyRP_OPT123_2025/MyRP_OPT123_2025.ino
@@ -1,11 +1,16 @@
 #include <my2wd_encoderOPT.h>
 //---------------------------------\\
 
+int servo_28_close = 148;   // แขนขวา
+int servo_27_close = 145;   // แขนซ้าย
+
+int trim_servo_0 = 10;
+int trim_servo_1 = 3;
 
 void setup()
   {
     Acceptable_values_moveLR(1.0,  5);     //------->> ตั้งค่า error  output ที่ยอมรับได้ในการหมุน
-    set_pid_moveLR(1.50, 0.0001, 0.0450);   //------->> ตั้งค่า pid สำหรับหมุนตัว
+    set_pid_moveLR(1.50, 0.00015, 0.045);   //------->> ตั้งค่า pid สำหรับหมุนตัว
     set_move_before_moveLR(190);   //------->> ตั้งค่า เดินหน้า ถอยหลัง ก่อนการหมุน
 
     setup_OPT();
@@ -13,17 +18,13 @@ void setup()
     
 
     //servo(29, 90);
-    /*
+    
     _servo(0, 90);     //----->แขนซ้ายชี้ไปข้างหน้า
     _servo(1, 90);     //----->แขนขวาชี้ไปข้างหน้า
 
-    servo(27, servo_27_close);   
-    servo(28, servo_28_close);servo(27, servo_27_close);   
-    servo(28, servo_28_close);
-    delay(100); 
-    servo(27, servo_27_close);   
-    servo(28, servo_28_close);
-    */
+    servo(27, servo_27_close-100);   
+    servo(28, servo_28_close-100); 
+    
     servo(29, 90);
     robot_start();
     eep_to_code();
@@ -31,9 +32,13 @@ void setup()
 
     //reset_arm();
     ///----------------------------------------------------->> เขียนโค๊ดที่นี้
-     st1();
 
-     
+      arm_Slide(1000);  //----->>  เลื่อนแขนขึ้น หน่วงเวลา 400
+      //arm_Slide(-400);  //----->>  เลื่อนแขนลงหน่วงเวลา 400
+      //arm_Slide(0);  //----->>  เลื่อนแขนลงให้สุดเพื่อเริ่มต้น
+
+
+
 
     ///----------------------------------------------------->> เขียนโค๊ดที่นี้
     ///----------------------------------------------------->> เขียนโค๊ดที่นี้
@@ -41,12 +46,10 @@ void setup()
   }
 
 void loop() {
-  
-   //servo(29, 0); delay(500);
-   //servo(29, 90); delay(500);
-   //servo(29, 180); delay(500);
-   //servo(29, 90); delay(500);
-  Serial.print(digitalRead(20));Serial.print("  ");  //อ่านค่าลิมิตสวิทย์
-  Serial.print(analogRead(26));Serial.println("  ");      //อ่านค่าวัดระย
-  //Serial.println(my.gyro('z')); delay(10);              //อ่านค่า gyro
+
+  Serial.print(digitalRead(20));Serial.println("  ");  //อ่านค่าลิมิตสวิทย์
+  //Serial.print(analogRead(26));Serial.println("  ");      //อ่านค่าวัดระย
+  Serial.println(my.gyro('z')); delay(10);              //อ่านค่า gyro
+  //Serial.print(encoder.Poss_L());Serial.print("  ");
+  //Serial.print(encoder.Poss_R());Serial.println("  ");
 }
