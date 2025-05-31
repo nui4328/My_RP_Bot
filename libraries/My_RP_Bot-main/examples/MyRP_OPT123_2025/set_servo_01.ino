1706dfb9ec1543dff4987d01c1005fb968f2dbe8
mr.chaiwat chainvong <65877075+nui4328@users.noreply.github.com>
2025-05-31 15:01:59 +0700

    Update set_servo_01.ino

--- libraries/My_RP_Bot-main/examples/MyRP_OPT123_2025/set_servo_01.ino
+++ libraries/My_RP_Bot-main/examples/MyRP_OPT123_2025/set_servo_01.ino
@@ -7,13 +7,13 @@ void _servo(int servo,int angle)
   {  
     if (servo==0)
       {
-          servo_pico0.attach(sevopico0, 500, 2500);
-          servo_pico0.write(angle);        
+          servo_pico0.attach(sevopico0, 0, 2640);
+          servo_pico0.write(angle + trim_servo_0);        
       }                                             //------>> ฟังก์ชัน สำหรับใช้งาน SERVO pin 0 และ 1
     else if (servo==1)
       {
           servo_pico1.attach(sevopico1,500, 2500);
-          servo_pico1.write(angle+5);      
+          servo_pico1.write(angle + trim_servo_1);      
       }
   }
 
