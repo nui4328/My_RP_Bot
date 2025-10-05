8e13b52c6d43e52dc1b9a1524c7e61e355117460
mr.chaiwat chainvong <65877075+nui4328@users.noreply.github.com>
2025-10-05 17:27:33 +0700

    Update ReadMe.ino

--- libraries/MyRP_ProV2/examples/MyRP_Pro2350/ReadMe.ino
+++ libraries/MyRP_ProV2/examples/MyRP_Pro2350/ReadMe.ino
@@ -1,3 +1,20 @@
+void set_motor()
+  {
+    set_Freq("Coreless_Motors");      // กำหนดความถี่ให้กับมอเตอร์เกาหลีหรือ มอเตอร์ Coreless ("Coreless_Motors")
+      //set_Freq("DC_Motors");          // กำหนดความถี่ให้กับมอเตอร์ธรรมกา(DC_Motors)
+
+    distance_scale(1.55);
+   
+    set_slow_motor(20, 20);     
+    set_turn_center_l(90, 90);
+    set_turn_center_r(90, 90);
+    set_turn_front_l(-15, 100);
+    set_turn_front_r(100, -15);
+    set_brake_fc(5, 20);
+    set_brake_bc(5, 20);
+    set_delay_f(10);
+    
+  }
 void readme()
   {
 
@@ -149,5 +166,6 @@ void readme()
 
 
   }
+
 
 
